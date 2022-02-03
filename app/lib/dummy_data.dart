42feebd71a6c53df9c26e5184d1db496e3f0762f
Seunghwanly <seunghwanly@gmail.com>
2022-02-03 18:05:31 +0900

    chore: added test dummy data
    image urls are from pixabay with free image

--- app/lib/dummy_data.dart
+++ app/lib/dummy_data.dart
@@ -1,40 +1,80 @@
-import 'package:app/floating_chat.dart';
+import 'package:app/bubble.dart';
 
-FloatingChat dummyCenter = FloatingChat(
-    name: "center",
-    distance: 0.0,
-    radius: 35.0,
-    hasUpdate: true,
-    top: 0,
-    left: 0);
-
-List<FloatingChat> dummyChats = [
-  FloatingChat(
-      name: "test1",
-      distance: 7.0,
-      radius: 35.0,
-      hasUpdate: false,
-      top: 0,
-      left: 0),
-  FloatingChat(
-      name: "test2",
-      distance: 16.0,
-      radius: 43.0,
-      hasUpdate: true,
-      top: 0.0,
-      left: 0.0),
-  FloatingChat(
-      name: "test3",
-      distance: 8.0,
-      radius: 27.0,
-      hasUpdate: false,
-      top: 0.0,
-      left: 0.0),
-  FloatingChat(
-      name: "test4",
-      distance: 54.0,
-      radius: 18.0,
-      hasUpdate: true,
-      top: 0.0,
-      left: 0.0),
+List<BubbleInformation> dummyData = [
+  BubbleInformation(
+    distance: 0,
+    size: 100,
+    updates: 2,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2017/04/01/21/06/portrait-2194457_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 10,
+    size: 80,
+    updates: 4,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 20,
+    size: 120,
+    updates: 6,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2014/07/31/22/50/photographer-407068_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 5,
+    size: 140,
+    updates: 2,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 80,
+    size: 60,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 6,
+    size: 60,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2017/02/16/23/10/smile-2072907_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 11,
+    size: 80,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2018/11/08/23/52/man-3803551_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 12,
+    size: 160,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2019/02/11/20/20/woman-3990680_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 7,
+    size: 100,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2017/11/06/13/45/cap-2923682_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 55,
+    size: 100,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2015/07/20/12/57/ambassador-852766_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 33,
+    size: 100,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2016/11/29/11/24/woman-1869158_1280.jpg',
+  ),
+  BubbleInformation(
+    distance: 40,
+    size: 100,
+    imageURL:
+        'https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_1280.jpg',
+  ),
 ];
